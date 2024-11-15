9326cc07d6e57b9d263d9477ac3d6519d72a70b2
Michal Valo <86716185+myzodev@users.noreply.github.com>
2024-11-15 20:48:41 +0100

    fix: article middleware permission

--- middlewares/article-middleware.js
+++ middlewares/article-middleware.js
@@ -8,7 +8,7 @@ export const doesHavePermissionToDelete = async (req, res, next) => {
         return next()
     }
 
-    const article = Article.find({ id })
+    const article = await Article.find({ id })
 
     if (article.user_id === isUserLoggedIn.id) {
         return next()
